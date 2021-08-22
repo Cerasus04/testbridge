db462167e982c782049f57e905b0cb9f4ac9aea1
cerasus1004 <cerasus1004@gmail.com>
2021-08-22 17:49:57 +0300

    template

--- build/js/main.js
+++ build/js/main.js
@@ -1,21 +1,21 @@
-// 'use strict';
+'use strict';
 
-// (function () {
-//   const anchors = document.querySelectorAll('a[href*="#"]');
+(function () {
+  const anchors = document.querySelectorAll('a[href*="#"]');
 
-//   for (let anchor of anchors) {
-//     anchor.addEventListener('click', (evt) => {
-//       evt.preventDefault();
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', (evt) => {
+      evt.preventDefault();
 
-//       const blockID = anchor.getAttribute('href').substr(1);
+      const blockID = anchor.getAttribute('href').substr(1);
 
-//       document.getElementById(blockID).scrollIntoView({
-//         behavior: 'smooth',
-//         block: 'start'
-//       });
-//     });
-//   }
-// })();
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  }
+})();
 
 'use strict';
 
